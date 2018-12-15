57dea3ce5bb11d4b4f5d4cc1dabc90c37b01f789
k183344@edu.tut.ac.jp <k183344@im106.mac.network>
2018-12-15 18:14:06 +0900

    testcase

--- s4/B183344/InformationEstimator.java
+++ s4/B183344/InformationEstimator.java
@@ -44,7 +44,7 @@ public class InformationEstimator implements InformationEstimatorInterface{
 	boolean [] partition = new boolean[myTarget.length+1];
 	int np;
 	np = 1<<(myTarget.length-1);
-	// System.out.println("np="+np+" length="+myTarget.length);
+	 System.out.println("np="+np+" length="+myTarget.length);
 	double value = Double.MAX_VALUE; // value = mininimum of each "value1".
 
 	for(int p=0; p<np; p++) { // There are 2^(n-1) kinds of partitions.
